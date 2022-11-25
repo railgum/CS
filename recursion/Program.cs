a9bdbc315f3476bc87b62ef04278ae923ecbe478
Rail <railbolo@gmail.com>
2022-11-25 17:10:19 +0400

    recursion

---
+++ recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿// Сумма чисел от 1 до n
+
+int SumFor(int n)
+{
+  int result = 0;
+  for (int i = 0; i <= n; i++)
+  {
+    result += i;
+  }
+  return result;
+}
+int SumRec(int n)
+{
+  if (n == 0) return 0;
+  else return n + SumRec(n - 1);
+}
+
+// Факториал числа
+
+int FactorialFor(int n)
+{
+  int result2 = 1;
+  for (int i = 0; i <= n; i++)
+  {
+    result2 *= i;
+  }
+  return result2;
+}
+
+int FactorialFRec(int n)
+{
+  if (n == 1) return 1;
+  else return n * FactorialFRec(n - 1);
+}
